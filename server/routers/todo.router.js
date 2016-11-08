e418e3d4ecb71c6c6091b074f589f959c712cb8a
johned <johned.matt@gmail.com>
2016-11-08 18:37:10 -0500

    created a new route for GET

--- server/routers/todo.router.js
+++ server/routers/todo.router.js
@@ -7,7 +7,18 @@ var mongoose = require('mongoose');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
-router.get('/todos', function(req, res){});
+router.get('/todos', function(req, res){
+  Todo.find({}, function(err, foundTodos){ //searches for db, if found names it foundTodos and send as oject
+    if(err){
+      res.status(500).json({
+        err: err
+      });
+    }
+    res.status(200).json({
+      todos: foundTodos
+    });
+  });
+});
 router.get('/todos/:id', function(req,res){});
 router.post('/todos', function(req, res){
   var todo = new Todo(req.body);
